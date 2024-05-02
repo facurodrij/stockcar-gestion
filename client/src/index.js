5d8ac3c3c1ddf876f3b908df2c805e3009a36309
FacuRodriJ <facurodri00@gmail.com>
2024-05-02 16:19:48 -0300

    App.css and AppBar.jsx removed; CssBaseLine and ThemeProvider added

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import CssBaseline from "@mui/material/CssBaseline";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import {theme} from "./theme";
+import 'dayjs/locale/es';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App/>
+        <CssBaseline/>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
     </React.StrictMode>
 );
---
+++ client/src/theme.jsx
@@ -0,0 +1,13 @@
+import {createTheme} from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#3f51b5',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+});
