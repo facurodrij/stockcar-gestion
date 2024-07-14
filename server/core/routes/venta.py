a919d28e9aa3a776dfe1c03251df651080d0b6bb
FacuRodriJ <facurodri00@gmail.com>
2024-07-13 22:10:03 -0300

    Venta request PUT updated

--- server/core/routes/venta.py
+++ server/core/routes/venta.py
@@ -108,7 +108,51 @@ def update(pk):
                         'renglones': list(map(lambda x: x.to_json(), venta_items))}), 200
     if request.method == 'PUT':
         data = request.json
-        for key, value in data.items():
+        venta_json = data['venta']
+        for key, value in venta_json.items():
             if value == '':
-                data[key] = None
-        return jsonify({'message': 'Venta actualizada'}), 200
+                venta_json[key] = None
+        venta_json['fecha_hora'] = datetime.fromisoformat(venta_json['fecha_hora'])
+        # venta_json['vencimiento_cae'] = datetime.fromisoformat(venta_json['vencimiento_cae']) if venta_json['vencimiento_cae'] else None
+        venta_json['nombre_cliente'] = Cliente.query.get(venta_json['cliente_id']).razon_social
+        venta_json['total'] = 100
+        venta_json['punto_venta'] = 1
+        venta_json['numero'] = 1
+
+        for key, value in venta_json.items():
+            setattr(venta, key, value)
+
+        current_articulo_ids = list(map(lambda x: x.articulo_id, venta_items))
+        renglones = data['renglones']
+        for item in renglones:
+            articulo_id = item['articulo_id']
+            if articulo_id in current_articulo_ids:
+                venta_item = VentaItem.query.filter_by(venta_id=pk, articulo_id=articulo_id).first()
+                for key, value in item.items():
+                    setattr(venta_item, key, value)
+                current_articulo_ids.remove(articulo_id)
+            else:
+                articulo = Articulo.query.get(articulo_id)
+                venta.items.append(VentaItem(
+                    articulo=articulo,
+                    descripcion=item['descripcion'],
+                    cantidad=item['cantidad'],
+                    precio_unidad=item['precio_unidad'],
+                    subtotal_iva=0,
+                    subtotal_gravado=100,
+                    subtotal=100
+                ))
+        for articulo_id in current_articulo_ids:
+            venta_item = VentaItem.query.filter_by(venta_id=pk, articulo_id=articulo_id).first()
+            db.session.delete(venta_item)
+
+        # nuevo_item = VentaItem(
+        #     articulo=articulo,
+        #     **{key: value for key, value in item_data.items() if key != 'id'}
+        # )
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({'error': str(e)}), 400
+        return 'ok', 200
