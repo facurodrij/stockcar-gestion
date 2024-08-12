272c3318af3207ba89b8f0a736af8f5509aaf2c3
FacuRodriJ <facurodri00@gmail.com>
2024-08-12 15:17:27 -0300

    Update VentaDetail to handle print invoice.

--- client/src/components/venta/VentaDetail.jsx
+++ client/src/components/venta/VentaDetail.jsx
@@ -1,9 +1,69 @@
 import React, { useEffect, useState } from 'react';
 import { API } from "../../App";
+import {
+    Box,
+    Button,
+    Grid,
+    Paper,
+    Typography,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    List,
+    ListItem,
+    ListItemText,
+    Menu,
+    MenuItem
+} from "@mui/material";
+import {Link} from "react-router-dom";
 import SnackbarAlert from "../shared/SnackbarAlert";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+
+const PaperSizeButton = ({ handlePrint }) => {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = (size) => {
+        setAnchorEl(null);
+        if (size) {
+            handlePrint(size);
+        }
+    };
+
+    return (
+        <>
+            <Button
+                variant="contained"
+                color="primary"
+                sx={{ ml: 2 }}
+                onClick={handleClick}
+                endIcon={<KeyboardArrowDownIcon />}
+            >
+                Imprimir
+            </Button>
+            <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={() => handleClose(null)}
+            >
+                <MenuItem onClick={() => handleClose('A4')}>A4</MenuItem>
+                <MenuItem onClick={() => handleClose('Ticket')}>Ticket</MenuItem>
+            </Menu>
+        </>
+    );
+};
+
 
 export default function VentaDetail({ pk }) {
     const [venta, setVenta] = useState({});
+    const [renglones, setRenglones] = useState([]);
     const [loading, setLoading] = useState(true);
     const [openSnackbar, setOpenSnackbar] = useState(false);
     const [snackbar, setSnackbar] = useState({
@@ -24,15 +84,17 @@ export default function VentaDetail({ pk }) {
             const url = `${API}/ventas/${pk}`;
             const res = await fetch(url);
             if (!res.ok) {
-                console.error(res);
-                throw new Error('Error al obtener la venta:', res);
+                throw new Error('Error al obtener la venta');
             }
             return await res.json();
         }
         const loadData = async () => {
             try {
                 const data = await fetchData();
-                setVenta(data);
+                setVenta(data['venta']);
+                setRenglones(data['renglones']);
+                console.log(venta);
+                console.log(renglones);
             }
             catch (error) {
                 setSnackbar({
@@ -46,12 +108,165 @@ export default function VentaDetail({ pk }) {
                 setLoading(false);
             }
         }
-
         loadData();
     }, [pk]);
 
+    const handlePrint = (size = 'A4') => {
+        const url = `${API}/ventas/${pk}/pdf?size=${size}`;
+        fetch(url)
+            .then(response => response.blob())
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+                window.open(url, '_blank');
+            })
+            .catch(() => {
+                setSnackbar({
+                    message: 'Error al imprimir la venta',
+                    severity: 'error',
+                    onClose: () => handleCloseSnackbar(false)
+                });
+                setOpenSnackbar(true);
+            });
+    }
+
     return (
         <>
+            <Paper elevation={3} component="div" sx={{ mt: 2, padding: 2 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Typography variant="h5" component="div" gutterBottom>
+                        Venta: {venta.id}
+                    </Typography>
+                    <Box>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            component={Link}
+                            to={`/ventas/form/${pk}`}
+                        >
+                            Editar
+                        </Button>
+                        <PaperSizeButton handlePrint={handlePrint} />
+                    </Box>
+                </Box>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <Paper elevation={1} component="div" sx={{ mt: 2, paddingTop: 2 }}>
+                            <Typography sx={{ ml: 2 }} variant="h6" component="div">
+                                Comprobante: {
+                                    venta.tipo_comprobante && venta.tipo_comprobante['descripcion']
+                                        ? venta.tipo_comprobante['descripcion']
+                                        : 'Descripción no disponible'
+                                }
+                            </Typography>
+                            <List sx={{ width: '100%' }}>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12} md={6}>
+                                        <ListItem>
+                                            <ListItemText primary="Fecha" secondary={venta.fecha_hora} />
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListItemText primary="Punto de venta" secondary={venta.punto_venta} />
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListItemText primary="Número" secondary={venta.numero} />
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListItemText primary="Estado" secondary={venta.estado} />
+                                        </ListItem>
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <ListItem>
+                                            <ListItemText primary="Nro. de comprobante" secondary={venta.nro_comprobante} />
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListItemText primary="CAE" secondary={venta.cae} />
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListItemText primary="Vencimiento de CAE" secondary={venta.vencimiento_cae} />
+                                        </ListItem>
+                                    </Grid>
+                                </Grid>
+                            </List>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Paper elevation={1} component="div" sx={{ mt: 2, paddingTop: 2 }}>
+                            <Typography sx={{ ml: 2 }} variant="h6" component="div">
+                                Cliente: {
+                                    venta.cliente && venta.cliente['razon_social']
+                                        ? venta.cliente['razon_social']
+                                        : 'Nombre no disponible'
+                                }
+                            </Typography>
+                            <List sx={{ width: '100%' }}>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12} md={6}>
+                                        <ListItem>
+                                            <ListItemText primary="CUIT" secondary={venta.cliente && venta.cliente['cuit'] ? venta.cliente['cuit'] : 'CUIT no disponible'} />
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListItemText primary="Condición de IVA" secondary={venta.cliente && venta.cliente['condicion_iva'] ? venta.cliente['condicion_iva'] : 'Condición de IVA no disponible'} />
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListItemText primary="Domicilio" secondary={venta.cliente && venta.cliente['domicilio'] ? venta.cliente['domicilio'] : 'Domicilio no disponible'} />
+                                        </ListItem>
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <ListItem>
+                                            <ListItemText primary="Localidad" secondary={venta.cliente && venta.cliente['localidad'] ? venta.cliente['localidad'] : 'Localidad no disponible'} />
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListItemText primary="Provincia" secondary={venta.cliente && venta.cliente['provincia'] ? venta.cliente['provincia'] : 'Provincia no disponible'} />
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListItemText primary="Código postal" secondary={venta.cliente && venta.cliente['codigo_postal'] ? venta.cliente['codigo_postal'] : 'Código postal no disponible'} />
+                                        </ListItem>
+                                    </Grid>
+                                </Grid>
+                            </List>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12} md={10}>
+                        <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>Renglones de Venta</Typography>
+                        <TableContainer component={Paper}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Descripción</TableCell>
+                                        <TableCell>Cantidad</TableCell>
+                                        <TableCell>Precio unitario</TableCell>
+                                        <TableCell>Subtotal</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {renglones.map((renglon, index) => (
+                                        <TableRow key={index}>
+                                            <TableCell>{renglon.descripcion}</TableCell>
+                                            <TableCell>{renglon.cantidad}</TableCell>
+                                            <TableCell>{renglon.precio_unidad}</TableCell>
+                                            <TableCell>{renglon.subtotal}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Grid>
+                    <Grid item xs={12} md={2}>
+                        <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>Totales</Typography>
+                        <List sx={{ width: '100%' }}>
+                            <ListItem>
+                                <ListItemText primary="Subtotal" secondary={venta.subtotal} />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="IVA" secondary={venta.iva} />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Total" secondary={venta.total} />
+                            </ListItem>
+                        </List>
+                    </Grid>
+                </Grid>
+            </Paper>
             <SnackbarAlert
                 open={openSnackbar}
                 autoHideDuration={4000}
