d268ad40942a489fc1b5aee65b63e8a561ce5ca9
FacuRodriJ <facurodri00@gmail.com>
2024-08-16 10:26:41 -0300

    feat: Update login route and add profile route

--- server/core/routes/usuario.py
+++ server/core/routes/usuario.py
@@ -1,24 +1,31 @@
+from datetime import timedelta
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from server.config import app, db
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required,
+    get_jwt_identity,
+    get_jwt,
+)
 from server.core.models import Usuario
 
-usuario_bp = Blueprint('usuario_bp', __name__)
+usuario_bp = Blueprint("usuario_bp", __name__)
 
-# Ruta de Inicio de Sesión
-@app.route('/login', methods=['POST'])
+
+@usuario_bp.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-    user = Usuario.query.filter_by(email=data['email']).first()
-    if user and user.password == data['password']:  # En un caso real, verifica la contraseña hasheada
-        access_token = create_access_token(identity={'email': user.email})
+    user = Usuario.query.filter_by(email=data["email"]).first()
+    if (
+        user and user.password == data["password"]
+    ):  # En un caso real, verifica la contraseña hasheada
+        access_token = create_access_token(identity={"email": user.email})
         return jsonify(access_token=access_token), 200
     return jsonify({"message": "Invalid credentials"}), 401
 
-# Ruta Protegida
-@app.route('/profile', methods=['GET'])
+
+@usuario_bp.route("/profile", methods=["GET"])
 @jwt_required()
 def profile():
     current_user = get_jwt_identity()
-    user = Usuario.query.filter_by(email=current_user['email']).first()
+    user = Usuario.query.filter_by(email=current_user["email"]).first()
     return jsonify({"user": user.to_json()}), 200
