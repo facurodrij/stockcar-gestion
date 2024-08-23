c9023183ce5f48c9f94d501010dd4071a7a18ef0
FacuRodriJ <facurodri00@gmail.com>
2024-08-23 14:22:01 -0300

    Add missing return statements in checkAuth and checkRoles functions

--- client/src/utils/checkAuth.jsx
+++ client/src/utils/checkAuth.jsx
@@ -4,15 +4,17 @@ export const checkAuth = () => {
         const redirect_to = window.location.pathname;
         window.location.href = '/login?redirect_to=' + redirect_to;
     }
+    return true;
 };
 
 export const checkRoles = (roles) => {
     const rolesList = JSON.parse(localStorage.getItem('roles'));
     const is_superuser = localStorage.getItem('is_superuser');
     if (is_superuser === 'true') {
-        return;
+        return true;
     }
     if (!rolesList || !rolesList.some(role => roles.includes(role))) {
         window.location.href = '/unauthorized';
     }
+    return true;
 }
