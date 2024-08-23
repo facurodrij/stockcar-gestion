a16caaa3a68da278e8b9f545bebbb86e63de7583
FacuRodriJ <facurodri00@gmail.com>
2024-08-23 14:20:10 -0300

    Update header to show correctly Drawer items depends on role

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -71,42 +71,48 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
     }),
 );
 
-// Lista de pages and icons
 const pagesList = [
     {
         title: 'Ventas',
         icon: <PointOfSale />,
         path: '/ventas',
+        roles_required: ['admin', 'cobranza'],
     },
     {
         title: 'Ordenes de Venta',
         icon: <PointOfSale />,
         path: '/ventas-orden',
+        roles_required: [],
     },
     {
         title: 'Articulos',
         icon: <Inventory2 />,
         path: '/articulos',
+        roles_required: ['admin'],
     },
     {
         title: 'Clientes',
         icon: <People />,
         path: '/clientes',
+        roles_required: ['admin', 'cobranza'],
     },
     {
         title: 'Usuarios',
         icon: <Person />,
         path: '/usuarios',
+        roles_required: ['admin'],
     },
     {
         title: 'Comercios',
         icon: <Store />,
         path: '/comercios',
+        roles_required: ['admin'],
     },
     {
         title: 'Configuración',
         icon: <Settings />,
         path: '/config',
+        roles_required: ['admin'],
     },
 ];
 
@@ -114,6 +120,8 @@ export default function Header() {
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
     const [auth, setAuth] = React.useState(false);
+    const [roles, setRoles] = React.useState([]);
+    const [is_superuser, setIsSuperuser] = React.useState(false);
 
     const handleDrawerOpen = () => {
         setOpen(true);
@@ -142,10 +150,17 @@ export default function Header() {
 
     useEffect(() => {
         const token = localStorage.getItem('token');
-        if (token) {
+        const roleList = JSON.parse(localStorage.getItem('roles'));
+        const is_superuser = localStorage.getItem('is_superuser');
+        if (token && roleList) {
             setAuth(true);
+            setRoles(roleList);
+            if (is_superuser === 'true') {
+                setIsSuperuser(true);
+            }
         } else {
             setAuth(false);
+            setRoles([]);
         }
     }, []);
 
@@ -153,21 +168,21 @@ export default function Header() {
         <>
             <AppBar position="fixed" open={open}>
                 <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        sx={{
-                            marginRight: 5,
-                            ...(open && { display: 'none' }),
-                        }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Box sx={{ flexGrow: 1 }} />
                     {auth ? (
-                        <div>
+                        <>
+                            <IconButton
+                                color="inherit"
+                                aria-label="open drawer"
+                                onClick={handleDrawerOpen}
+                                edge="start"
+                                sx={{
+                                    marginRight: 5,
+                                    ...(open && { display: 'none' }),
+                                }}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Box sx={{ flexGrow: 1 }} />
                             <IconButton
                                 aria-label="account of current user"
                                 aria-controls="menu-appbar"
@@ -198,70 +213,53 @@ export default function Header() {
                                 <Divider />
                                 <MenuItem onClick={handleLogout}>Cerrar Sesión</MenuItem>
                             </Menu>
-                        </div>
+                        </>
                     ) : (
-                        <Button color="inherit" component={Link} to="/login">Login</Button>
+                        <>
+                        <Box sx={{ flexGrow: 1 }} />
+                        <Button color="inherit" component={Link} to="/login">Iniciar sesión</Button>
+                        </>
                     )}
                 </Toolbar>
             </AppBar>
-            <Drawer variant="permanent" open={open}>
-                <DrawerHeader>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-                    </IconButton>
-                </DrawerHeader>
-                <Divider />
-                <List>
-                    {pagesList.map((page, index) => (
-                        <ListItem key={page.title} disablePadding sx={{ display: 'block' }}>
-                            <ListItemButton
-                                component={Link} to={page.path}
-                                sx={{
-                                    minHeight: 48,
-                                    justifyContent: open ? 'initial' : 'center',
-                                    px: 2.5,
-                                }}
-                            >
-                                <ListItemIcon
-                                    sx={{
-                                        minWidth: 0,
-                                        mr: open ? 3 : 'auto',
-                                        justifyContent: 'center',
-                                    }}
-                                >
-                                    {page.icon}
-                                </ListItemIcon>
-                                <ListItemText primary={page.title} sx={{ opacity: open ? 1 : 0 }} />
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
-                <Divider />
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                            <ListItemButton
-                                sx={{
-                                    minHeight: 48,
-                                    justifyContent: open ? 'initial' : 'center',
-                                    px: 2.5,
-                                }}
-                            >
-                                <ListItemIcon
-                                    sx={{
-                                        minWidth: 0,
-                                        mr: open ? 3 : 'auto',
-                                        justifyContent: 'center',
-                                    }}
-                                >
-                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                </ListItemIcon>
-                                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer>
+            {auth && (
+                <Drawer variant="permanent" open={open}>
+                    <DrawerHeader>
+                        <IconButton onClick={handleDrawerClose}>
+                            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                        </IconButton>
+                    </DrawerHeader>
+                    <Divider />
+                    <List>
+                        {pagesList.map((page, index) => (
+                            is_superuser ||
+                                page.roles_required.length === 0 || page.roles_required.some(role => roles.includes(role)) ? (
+                                <ListItem key={page.title} disablePadding sx={{ display: 'block' }}>
+                                    <ListItemButton
+                                        component={Link} to={page.path}
+                                        sx={{
+                                            minHeight: 48,
+                                            justifyContent: open ? 'initial' : 'center',
+                                            px: 2.5,
+                                        }}
+                                    >
+                                        <ListItemIcon
+                                            sx={{
+                                                minWidth: 0,
+                                                mr: open ? 3 : 'auto',
+                                                justifyContent: 'center',
+                                            }}
+                                        >
+                                            {page.icon}
+                                        </ListItemIcon>
+                                        <ListItemText primary={page.title} sx={{ opacity: open ? 1 : 0 }} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ) : null
+                        ))}
+                    </List>
+                </Drawer>
+            )}
         </>
     );
 }
