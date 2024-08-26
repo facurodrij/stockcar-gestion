e1f64dc45f0047c8822497cd6938fefa36f72594
FacuRodriJ <facurodri00@gmail.com>
2024-08-26 15:03:07 -0300

    Update articulo route

--- server/core/routes/articulo.py
+++ server/core/routes/articulo.py
@@ -1,10 +1,11 @@
-from datetime import date, datetime, timedelta
 from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required
 
 from server.config import db
 from server.core.models import AlicuotaIVA, Tributo, Articulo, TipoArticulo, TipoUnidad
+from server.core.decorators import permission_required
 
-articulo_bp = Blueprint('articulo_bp', __name__)
+articulo_bp = Blueprint("articulo_bp", __name__)
 
 
 def get_select_options():
@@ -13,77 +14,90 @@ def get_select_options():
     alicuota_iva = AlicuotaIVA.query.all()
     tributo = Tributo.query.all()
     return {
-        'tipo_articulo': list(map(lambda x: x.to_json(), tipo_articulo)),
-        'tipo_unidad': list(map(lambda x: x.to_json(), tipo_unidad)),
-        'alicuota_iva': list(map(lambda x: x.to_json(), alicuota_iva)),
-        'tributo': list(map(lambda x: x.to_json(), tributo)),
+        "tipo_articulo": list(map(lambda x: x.to_json(), tipo_articulo)),
+        "tipo_unidad": list(map(lambda x: x.to_json(), tipo_unidad)),
+        "alicuota_iva": list(map(lambda x: x.to_json(), alicuota_iva)),
+        "tributo": list(map(lambda x: x.to_json(), tributo)),
     }
 
 
-@articulo_bp.route('/articulos', methods=['GET'])
+@articulo_bp.route("/articulos", methods=["GET"])
+@jwt_required()
+@permission_required(["articulo.view_all"])
 def index():
     articulos = Articulo.query.all()
     articulos_json = list(map(lambda x: x.to_json(), articulos))
-    return jsonify({'articulos': articulos_json}), 200
+    return jsonify({"articulos": articulos_json}), 200
 
 
-@articulo_bp.route('/articulos/create', methods=['GET', 'POST'])
+@articulo_bp.route("/articulos/create", methods=["GET", "POST"])
+@jwt_required()
+@permission_required(["articulo.create"])
 def create():
-    if request.method == 'GET':
-        return jsonify({'select_options': get_select_options()}), 200
-    if request.method == 'POST':
+    if request.method == "GET":
+        return jsonify({"select_options": get_select_options()}), 200
+    if request.method == "POST":
         data = request.json
-        articulo_json = data['articulo']
+        articulo_json = data["articulo"]
         for key, value in articulo_json.items():
-            if value == '':
+            if value == "":
                 articulo_json[key] = None
-
-        articulo = Articulo(**articulo_json)
-
         try:
+            articulo = Articulo(**articulo_json)
             db.session.add(articulo)
-            for tributo_id in data['tributos']:
+            for tributo_id in data["tributos"]:
                 tributo = Tributo.query.get_or_404(tributo_id)
                 articulo.tributos.append(tributo)
             db.session.commit()
+            return jsonify({"articulo_id": articulo.id}), 201
         except Exception as e:
             db.session.rollback()
-            return jsonify({'error': str(e)}), 400
-        return 'ok', 201
+            print(e)
+            return jsonify({"error": str(e)}), 400
+        finally:
+            db.session.close()
 
 
-@articulo_bp.route('/articulos/<int:pk>', methods=['GET'])
-def detail(pk):
-    articulo = Articulo.query.get_or_404(pk, 'Artículo no encontrado')
-    return jsonify({'articulo': articulo.to_json()}), 200
-
-
-@articulo_bp.route('/articulos/<int:pk>/update', methods=['GET', 'PUT'])
+@articulo_bp.route("/articulos/<int:pk>/update", methods=["GET", "PUT"])
+@jwt_required()
+@permission_required(["articulo.update"])
 def update(pk):
-    articulo = Articulo.query.get_or_404(pk, 'Artículo no encontrado')
-    if request.method == 'GET':
-        return jsonify({
-            'select_options': get_select_options(),
-            'articulo': articulo.to_json()
-        }), 200
-    if request.method == 'PUT':
+    articulo = Articulo.query.get_or_404(pk, "Artículo no encontrado")
+    if request.method == "GET":
+        return (
+            jsonify(
+                {"select_options": get_select_options(), "articulo": articulo.to_json()}
+            ),
+            200,
+        )
+    if request.method == "PUT":
         data = request.json
-        articulo_json = data['articulo']
+        articulo_json = data["articulo"]
         for key, value in articulo_json.items():
-            if value == '':
+            if value == "":
                 articulo_json[key] = None
-
-        for key, value in articulo_json.items():
-            setattr(articulo, key, value)
-
-        articulo.tributos = []
-        nuevos_tributos = Tributo.query.filter(Tributo.id.in_(data['tributos'])).all()
-        for tributo in nuevos_tributos:
-            articulo.tributos.append(tributo)
-
         try:
+            for key, value in articulo_json.items():
+                setattr(articulo, key, value)
+            articulo.tributos = []
+            nuevos_tributos = Tributo.query.filter(
+                Tributo.id.in_(data["tributos"])
+            ).all()
+            for tributo in nuevos_tributos:
+                articulo.tributos.append(tributo)
             db.session.commit()
+            return jsonify({"articulo_id": articulo.id}), 201
         except Exception as e:
             db.session.rollback()
-            return jsonify({'error': str(e)}), 400
-        return 'ok', 201
+            print(e)
+            return jsonify({"error": str(e)}), 400
+        finally:
+            db.session.close()
+
+
+@articulo_bp.route("/articulos/<int:pk>", methods=["GET"])
+@jwt_required()
+@permission_required(["articulo.view"])
+def detail(pk):
+    articulo = Articulo.query.get_or_404(pk, "Artículo no encontrado")
+    return jsonify({"articulo": articulo.to_json()}), 200
