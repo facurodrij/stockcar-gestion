3dd1647cb49de364471aff8a7a9cf8a3b26079a1
FacuRodriJ <facurodri00@gmail.com>
2024-08-26 12:43:33 -0300

    Add permission_required decorator to  check for user permissions

--- server/core/decorators.py
+++ server/core/decorators.py
@@ -1,21 +1,22 @@
 from functools import wraps
 from flask import request, jsonify
 from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
-from server.core.models import Usuario, Rol
-from server.config import jwt
+from server.core.models import Usuario
+
 
 def admin_required(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
         verify_jwt_in_request()
         current_user = get_jwt_identity()
-        user = Usuario.query.filter_by(email=current_user.get('email')).first()
-        if user.is_superuser or user.is_staff:
+        user = Usuario.query.filter_by(email=current_user.get("email")).first()
+        if user.is_superuser:
             return fn(*args, **kwargs)
         # Si el usuario tiene el rol con nombre 'admin' puede acceder
-        if 'admin' in [rol.nombre for rol in user.roles]:
+        if "admin" in [rol.nombre for rol in user.roles]:
             return fn(*args, **kwargs)
-        return jsonify({'error': 'Sin permisos para acceder a este recurso'}), 403
+        return jsonify({"error": "Sin permisos para acceder a este recurso"}), 403
+
     return wrapper
 
 
@@ -25,14 +26,34 @@ def role_required(roles: list):
         def wrapper(*args, **kwargs):
             verify_jwt_in_request()
             current_user: dict = get_jwt_identity()
-            user = Usuario.query.filter_by(email=current_user.get('email')).first()
+            user = Usuario.query.filter_by(email=current_user.get("email")).first()
             # Si el usuario es superusuario o staff, no se verifica el rol
-            if user.is_superuser or user.is_staff:
+            if user.is_superuser:
                 return fn(*args, **kwargs)
             for role in roles:
                 if role in [rol.nombre for rol in user.roles]:
                     return fn(*args, **kwargs)
-            return jsonify({'error': 'Sin permisos para acceder a este recurso'}), 403
+            return jsonify({"error": "Sin permisos para acceder a este recurso"}), 403
+
         return wrapper
+
     return decorator
 
+
+def permission_required(permissions: list):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            verify_jwt_in_request()
+            current_user: dict = get_jwt_identity()
+            user = Usuario.query.filter_by(email=current_user.get("email")).first()
+            if user.is_superuser:
+                return fn(*args, **kwargs)
+            for permission in permissions:
+                if permission in [perm.nombre for perm in user.permisos]:
+                    return fn(*args, **kwargs)
+            return jsonify({"error": "Sin permisos para acceder a este recurso"}), 403
+
+        return wrapper
+
+    return decorator
