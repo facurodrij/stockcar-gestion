e919b847556ab7aaa4e0382c13112cfc4fd8ec67
FacuRodriJ <facurodri00@gmail.com>
2024-09-05 12:09:46 -0300

    Update ArticuloList component to use useEffect and useState hooks consistently

--- client/src/components/articulo/ArticuloList.jsx
+++ client/src/components/articulo/ArticuloList.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     DataGrid,
     GridActionsCellItem,
@@ -9,26 +9,28 @@ import {
     GridToolbarFilterButton, GridToolbarQuickFilter
 } from '@mui/x-data-grid';
 import EditIcon from '@mui/icons-material/Edit';
-import {API} from "../../App";
-import {Link} from "react-router-dom";
-import {esES} from "@mui/x-data-grid/locales";
-import {Button} from "@mui/material";
+import { API } from "../../App";
+import { Link } from "react-router-dom";
+import { esES } from "@mui/x-data-grid/locales";
+import { Box, Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import Box from "@mui/material/Box";
 import fetchWithAuth from '../../utils/fetchWithAuth';
+import SnackbarAlert from '../shared/SnackbarAlert';
+import { useLoading } from '../../utils/loadingContext';
+
 
 
 const CustomToolbar = () => {
     return (
         <GridToolbarContainer>
-            <GridToolbarQuickFilter size={'small'}/>
-            <GridToolbarColumnsButton/>
-            <GridToolbarFilterButton/>
-            <GridToolbarDensitySelector/>
-            <GridToolbarExport/>
+            <GridToolbarQuickFilter size={'small'} />
+            <GridToolbarColumnsButton />
+            <GridToolbarFilterButton />
+            <GridToolbarDensitySelector />
+            <GridToolbarExport />
             <Box sx={{ flexGrow: 1 }} />
             <Button
-                startIcon={<AddIcon/>}
+                startIcon={<AddIcon />}
                 component={Link}
                 to="/articulos/form"
                 size="small"
@@ -43,38 +45,56 @@ const CustomToolbar = () => {
 
 export default function ArticuloList() {
     const [list, setList] = useState([]);
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+    const [snackbar, setSnackbar] = useState({
+        message: '',
+        severity: 'success',
+        autoHideDuration: 4000,
+        onClose: () => handleCloseSnackbar(false)
+    });
+    const { withLoading } = useLoading();
 
-    const fetchData = async () => {
-        const url = `${API}/articulos`;
-        try {
-            const res = await fetchWithAuth(url);
-            const data = await res.json();
-            if (!res.ok) {
-                throw new Error(`${res.status} (${res.statusText})`);
-            }
-            setList(data['articulos']);
-        } catch (error) {
-            console.error(error);
-            alert('Error al cargar los datos');
-        }
-    }
+    const handleCloseSnackbar = () => {
+        setOpenSnackbar(false);
+    };
 
     useEffect(() => {
-        fetchData();
-    }, []);
+        const fetchData = async () => {
+            const url = `${API}/articulos`;
+            try {
+                const res = await fetchWithAuth(url);
+                const data = await res.json();
+                if (!res.ok) {
+                    throw new Error(data['error']);
+                }
+                setList(data['articulos']);
+            } catch (error) {
+                console.error(error);
+                setSnackbar({
+                    message: `Error al obtener los artículos: ${error.message}`,
+                    severity: 'error',
+                    autoHideDuration: null,
+                    onClose: handleCloseSnackbar
+                });
+                setOpenSnackbar(true);
+            }
+        }
+
+        withLoading(fetchData);
+    }, [withLoading]);
 
     const columns = [
-        {field: 'descripcion', headerName: 'Descripción', flex: 2},
-        {field: 'codigo_principal', headerName: 'Código principal', flex: 1},
-        {field: 'codigo_secundario', headerName: 'Código secundario', flex: 0.75},
-        {field: 'codigo_terciario', headerName: 'Código terciario', flex: 0.75},
-        {field: 'codigo_cuaternario', headerName: 'Código cuaternario', flex: 0.75},
-        {field: 'codigo_adicional', headerName: 'Código adicional', flex: 0.75},
+        { field: 'descripcion', headerName: 'Descripción', flex: 2 },
+        { field: 'codigo_principal', headerName: 'Código principal', flex: 1 },
+        { field: 'codigo_secundario', headerName: 'Código secundario', flex: 0.75 },
+        { field: 'codigo_terciario', headerName: 'Código terciario', flex: 0.75 },
+        { field: 'codigo_cuaternario', headerName: 'Código cuaternario', flex: 0.75 },
+        { field: 'codigo_adicional', headerName: 'Código adicional', flex: 0.75 },
         {
             field: 'actions', type: 'actions', headerName: 'Acciones', flex: 0.5,
             getActions: (params) => [
                 <GridActionsCellItem
-                    icon={<EditIcon/>}
+                    icon={<EditIcon />}
                     label="Editar"
                     component={Link}
                     to={`/articulos/form/${params.row.id}`}
@@ -97,19 +117,28 @@ export default function ArticuloList() {
     });
 
     return (
-        <div style={{height: 500, width: '100%'}}>
-            <DataGrid
-                columns={columns}
-                rows={rows}
-                disableRowSelectionOnClick
-                rowHeight={30}
-                pageSize={5}
-                rowsPerPageOptions={[5, 10, 20]}
-                initialState={{sorting: {sortModel: [{field: 'id', sort: 'desc'}]}}}
-                localeText={esES.components.MuiDataGrid.defaultProps.localeText}
-                slots={{toolbar: CustomToolbar}}
-                ignoreDiacritics
+        <>
+            <div style={{ height: 500, width: '100%' }}>
+                <DataGrid
+                    columns={columns}
+                    rows={rows}
+                    disableRowSelectionOnClick
+                    rowHeight={30}
+                    pageSize={5}
+                    rowsPerPageOptions={[5, 10, 20]}
+                    initialState={{ sorting: { sortModel: [{ field: 'id', sort: 'desc' }] } }}
+                    localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+                    slots={{ toolbar: CustomToolbar }}
+                    ignoreDiacritics
+                />
+            </div>
+            <SnackbarAlert
+                open={openSnackbar}
+                message={snackbar.message}
+                severity={snackbar.severity}
+                autoHideDuration={snackbar.autoHideDuration}
+                onClose={snackbar.onClose}
             />
-        </div>
+        </>
     );
 };
