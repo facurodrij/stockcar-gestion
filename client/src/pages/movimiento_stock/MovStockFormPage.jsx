cf290ee3785ce9dbb1c9bdb718b66869a57fb720
FacuRodriJ <facurodri00@gmail.com>
2024-09-08 18:03:20 -0300

    Refactor MovStockFormPage component to remove unused import statements and simplify code

--- client/src/pages/movimiento_stock/MovStockFormPage.jsx
+++ client/src/pages/movimiento_stock/MovStockFormPage.jsx
@@ -1,16 +1,12 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import Typography from "@mui/material/Typography";
 import MovStockForm from '../../components/movimiento_stock/MovStockForm';
 import { checkAuth, checkPermissions } from '../../utils/checkAuth';
 
 export default function MovStockFormPage() {
-    const routeParams = useParams();
-    const pk = routeParams.pk;
-
     useEffect(() => {
         checkAuth();
-        if (!checkPermissions(['movimiento_stock.create', 'movimiento_stock.update'])) {
+        if (!checkPermissions(['movimiento_stock.create'])) {
             window.location.href = '/unauthorized';
         }
     }, []);
@@ -26,9 +22,9 @@ export default function MovStockFormPage() {
                     color: 'inherit'
                 }}
             >
-                {pk ? 'Editar' : 'Agregar'} Movimiento de Stock
+                Agregar Movimiento de Stock
             </Typography>
-            <MovStockForm pk={pk} />
+            <MovStockForm />
         </>
     );
 };
