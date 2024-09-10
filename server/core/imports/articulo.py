76f08ce1cb27c2d0bf644192cd59288eab781ea6
FacuRodriJ <facurodri00@gmail.com>
2024-09-10 16:41:36 -0300

    Refactor import_data function to include stock information for Articulo model

--- server/core/imports/articulo.py
+++ server/core/imports/articulo.py
@@ -9,31 +9,62 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 def import_data():
     print("Importing data for Articulo model...")
-    df = pd.read_json(os.path.join(basedir, '../../../dump/Producto.json'))
+    df = pd.read_json(os.path.join(basedir, "../../../dump/Producto.json"))
+    df_stock = pd.read_json(os.path.join(basedir, "../../../dump/Stock.json"))
 
-    df = df.rename(columns={
-        # Datos Principales
-        "PRO_CODIGO": "id",
-        "PRO_CODBAR": "codigo_barras",
-        "PRO_NOMBRE": "descripcion",
-        "PRO_EXISTENCIA": "stock_actual",
-    })
+    # Filtrar solo los productos que son deposito 1
+    df_stock = df_stock[df_stock["STK_DEPOSITO"] == 1]
+
+    df = df.rename(
+        columns={
+            "PRO_CODIGO": "id",
+            "PRO_CODBAR": "codigo_barras",
+            "PRO_NOMBRE": "descripcion",
+        }
+    )
+
+    df_stock = df_stock.rename(
+        columns={
+            "STK_PRODUCTO": "articulo_id",
+            "STK_EXISTENCIA": "existencia",
+        }
+    )
+
+    df["descripcion"] += (
+        " "
+        + df["PRO_LINEA1"].map(str)
+        + " "
+        + df["PRO_LINEA2"].map(str)
+        + " "
+        + df["PRO_LINEA3"].map(str)
+        + df["PRO_LINEA4"].map(str)
+        + " "
+        + df["PRO_LINEA5"].map(str)
+        + " "
+        + df["PRO_LINEA6"].map(str)
+        + " "
+        + df["PRO_LINEA7"].map(str)
+    )
 
     df = df.map(lambda x: x.strip() if isinstance(x, str) else x)
     df = df.replace({np.nan: None})
-
-    df['descripcion'] += ' ' + df['PRO_LINEA1'].map(str) + ' ' + df['PRO_LINEA2'].map(str) + ' ' + df['PRO_LINEA3'].map(
-        str) + df['PRO_LINEA4'].map(str) + ' ' + df['PRO_LINEA5'].map(str) + ' ' + df['PRO_LINEA6'].map(
-        str) + ' ' + df['PRO_LINEA7'].map(str)
+    df_stock = df_stock.replace({np.nan: None})
+    df_stock = df_stock.map(lambda x: x.strip() if isinstance(x, str) else x)
 
     articulo_list = []
     for index, row in df.iterrows():
+        # Obtener el stock actual del artículo
+        stock = df_stock[df_stock["articulo_id"] == row["id"]]
+        if len(stock) > 0:
+            row["existencia"] = stock["existencia"].values[0]
+        else:
+            row["existencia"] = 0
         articulo = Articulo(
-            id=row['id'],
-            codigo_principal=row['codigo_barras'],
-            descripcion=row['descripcion'],
-            linea_factura=row['descripcion'][:30],
-            stock_actual=row['stock_actual'],
+            id=row["id"],
+            codigo_principal=row["codigo_barras"],
+            descripcion=row["descripcion"],
+            linea_factura=row["descripcion"][:30],
+            stock_actual=row["existencia"],
         )
         articulo_list.append(articulo)
 
