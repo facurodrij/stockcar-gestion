17a8c3294d2af9d23b5f26ac2c9a0140e4bae1ca
FacuRodriJ <facurodri00@gmail.com>
2024-09-12 14:31:53 -0300

    Refactor start_servers.bat to use serve instead of npm start

--- start_servers.bat
+++ start_servers.bat
@@ -3,4 +3,4 @@ cd /d C:\stockcar-gestion\server
 call .venv\Scripts\activate
 start cmd /k "waitress-serve --host=192.168.0.19 --port=50100 --call wsgi:create_app"
 cd /d C:\stockcar-gestion\client
-start cmd /k "npm start"
+start cmd /k "serve -s build -l 3000"
